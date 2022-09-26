e2c482aa3009fb0e614c5aabe77735e660e25167
Rodolfo Zevallos <20051487+rjzevallos@users.noreply.github.com>
2022-09-26 21:18:47 +0200

    update util file

---
+++ notebook/README.md
@@ -0,0 +1,5 @@
+
+# ParlaMint Spanish Catalan Colab
+
+The Colab file is designed for simplified use of the repository scripts, however it works in the same way. To use it, use the file and follow the steps described in it.
+
--- src/util.py
+++ src/util.py
@@ -5,6 +5,7 @@ import pandas as pd
 import sys
 import spacy
 import re
+import ast
 
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
@@ -28,14 +29,28 @@ def process_test():
     text = fix_text(text)
     doc = nlp_ca(text)
     df_nlp = nlp_spacy(doc)
-    print(df_nlp)
+    #print(df_nlp)
     df_to_conll_new(df_nlp)
+    print(doc_final)
 
+def fix_dataframe(df):
+    for index, row in df.iterrows():
+        jo = str(row["join"])
+        token_join = ast.literal_eval(jo)
+        if len(token_join) > 0:
+            for indexs, rows in df.iterrows():
+                if int(rows["word_id"]) in token_join:
+                    for t_j in token_join:
+                        if int(rows["word_id"]) == t_j:
+                            df.at[(t_j-1),'join']="[0]"
+                            
+    return df
 
 def nlp_spacy(doc):
     rows_ner = []
     case_rare = ["a","d","p"]
     case_rare_next = ['l','ls','el','els']
+    count_word = 0
     for sentid, sentence in enumerate(doc.sents):
         sent_id = sentid
         flag_del = 0
@@ -52,14 +67,11 @@ def nlp_spacy(doc):
             deprel = token.dep_
             ent = token.ent_type_
             ent_cat = token.ent_iob_
-            join = '_'
+            join_word = []
             if token.ent_iob_ != 'O':
                 ner = token.ent_iob_ + '-' + token.ent_type_
             else:
                 ner = token.ent_iob_
-            print('token', text)
-            print(text.find("-"))
-            print('flag_verb', flag_verb)
             if (text in case_rare and pos == 'DET') or (text == 'p' and pos == "ADP"):
                 flag_del = 1
                 value_case_rare = text
@@ -69,30 +81,28 @@ def nlp_spacy(doc):
                 flag_del = 0
                 flag_add = 1
             elif pos == 'VERB' and flag_verb == 0:
-                print('verb')
                 flag_verb = 1
                 value_case_rare = text
             elif flag_verb == 1 and int(text.find("-")) == 0:
-                print('compuesto')
                 lemma_join = ''.join([value_case_rare,text])
                 text_join = ''.join([value_case_rare,text])
                 flag_verb = 0
                 flag_add = 1
             elif flag_verb == 1:
-                print('flag_verb a 0')
                 flag_verb = 0
-            
             if int(text.find("'")) == 0:
                 if text == "'n":
                     lemma = "ne"
-                    join = "yes"
+                    join_word.append(count_word+1)
             if text == "-lo":
                 lemma = "el"
             
             if len(lemma.split(" ")) > 1:
                 lemma = lemma.split(" ")[0]
             
+            count_word += 1
             row_ner = {
+              'word_id': count_word,
               'sent_id': sent_id,
               'token_id': id,
               'text': text,
@@ -102,13 +112,17 @@ def nlp_spacy(doc):
               'head': head,
               'deprel': deprel,
               'token_ner_type' : ner,
-              'join': join
+              'join': []
             }
             rows_ner.append(row_ner)
             df_ner = pd.DataFrame(rows_ner)
             
             if flag_add == 1:
+                count_word += 1
+                join_word.append(count_word-2)
+                join_word.append(count_word-1)
                 row_ner = {
+                  'word_id': count_word,
                   'sent_id': sent_id,
                   'token_id': id,
                   'text': text_join,
@@ -118,7 +132,7 @@ def nlp_spacy(doc):
                   'head': head,
                   'deprel': deprel,
                   'token_ner_type' : ner,
-                  'join': 'yes'
+                  'join': join_word
                 }
                 rows_ner.append(row_ner)
                 df_ner = pd.DataFrame(rows_ner)
@@ -140,7 +154,6 @@ def nlp_spacy(doc):
     df_ner['misc_'].fillna('_', inplace=True)
     return df_ner
 
-
 def df_to_conll_new(df_ner):
     conll_list = []
     flag = 0
@@ -156,11 +169,11 @@ def df_to_conll_new(df_ner):
             misc = "SpaceAfter=No"
         else:
             misc = "_"
-        if str(row["morph"]) == "()":
+        if str(row["morph"]) == "()" or str(row["morph"]) == "":
             morph = "_"
         else:
             morph = row["morph"]
-        row_conll = str(row["token_id"])+"\t"+str(row["text"])+"\t"+str(row["lemma"])+"\t"+str(row["pos"])+"\t"+str(row["pos"])+"\t"+str(morph)+"\t"+ str(heads)+"\t"+str(row["deprel"])+"\t"+str("_")+"\t"+str(misc)+"\t"+str(row["token_ner_type"])+"\t"+str(row["misc_"])+"\n"
+        row_conll = str(row["token_id"])+"\t"+str(row["text"])+"\t"+str(row["lemma"])+"\t"+str(row["pos"])+"\t"+str(row["pos"])+"\t"+str(morph)+"\t"+ str(heads)+"\t"+str(row["deprel"])+"\t"+str("_")+"\t"+str(misc)+"\t"+str(row["token_ner_type"])+"\t"+str(row["misc_"])+"\t"+str(row["join"])+"\n"
         conll_list.append(flag_add+row_conll)
     doc_final = ''.join([str(x) for x in conll_list])
     return doc_final
@@ -168,11 +181,14 @@ def df_to_conll_new(df_ner):
 
 def to_conll(doc):
     df_nlp = nlp_spacy(doc)
+    df_nlp = fix_dataframe(df_nlp)
+    print(df_nlp)
     nlp_conll = df_to_conll_new(df_nlp)
     return nlp_conll
 
 
 def generate_tei(seq,parent):
+    seq = ["Autoritats, senyores i senyors, bon dia i benvinguts al Parlament de Catalunya en aquesta sessió constitutiva de l'onzena legislatura y beneficiar-se'n"]
     seq=deepcopy(seq)
     attrib=deepcopy(parent.attrib)
     parent.clear()
@@ -180,104 +196,154 @@ def generate_tei(seq,parent):
     seg_id=attrib['{http://www.w3.org/XML/1998/namespace}id']
     seg_lang=attrib['{http://www.w3.org/XML/1998/namespace}lang']
     text=''
+    #print('la seq es: ', seq)
+    #print('el parent es: ', parent)
+    
     for e in seq:
-        print(e)
         if isinstance(e,str):
             text+=e
-    print("lang: ", seg_lang)
     text = fix_text(text)
     if seg_lang == "es":
-        print("Procensando en es")
         doc = nlp_es(text)
     elif seg_lang == "ca":
-        print("Procensando en ca")
         doc = nlp_ca(text)
-    
+    elif seg_lang == "en":
+        doc = nlp_es(text)
+    else:
+        doc = nlp_es(text)
     seq_idx=0
     str_idx=0
-    while not isinstance(seq[seq_idx],str): 
+    
+    #Casi no debe entrar aquí
+    while not isinstance(seq[seq_idx],str):
+        print('1 puerta')
         parent.append(seq[seq_idx])
         seq_idx+=1
         if seq_idx==len(seq):
             return
+    
     for sidx,sentence in enumerate(to_conll(doc).strip().split('\n\n')):
+        #print("el sidx es: ", sidx)
+        #print("la sentence es: ", sentence)
         s_id=seg_id+'.'+str(sidx+1)
         s = ET.Element('s',attrib={'xml:id':s_id, 'xml:lang': seg_lang})
+        #print('el atributo S es: ', s)
         parent.append(s)
         ne_cat='O' 
         cont_cat = '_' #nuevo
         parent_s = s
-        tokens = sentence.split('\n') #we separate the tokens from the sentences
-        dependencies=[] #a list to save the dependecies
+        tokens = sentence.split('\n')
+        dependencies=[] 
         for tidx,token in enumerate(tokens):
             print(token)
             if token.startswith('#'):
                 continue
-            token=token.split('\t') #we separate the token's attributes 
-            t_id=s_id+'.'+token[0] 
-            if token[6]=='0': #we check if the token is root
-                dependencies.append(('ud-syn:'+token[7],'#'+s_id+' #'+t_id)) #if the token is root, the target is the sentence id
+            token=token.split('\t')
+            token_12 = ast.literal_eval(token[12])
+            
+            
+            t_id=s_id+'.'+token[0]
+            if token[6]=='0':
+                dependencies.append(('ud-syn:'+token[7],'#'+s_id+' #'+t_id)) 
             else:
-                dependencies.append(('ud-syn:'+token[7],'#'+s_id+'.'+token[6]+' #'+t_id)) #if the token is not root, the target is composed with the dependency number
+                dependencies.append(('ud-syn:'+token[7],'#'+s_id+'.'+token[6]+' #'+t_id))
+            
             new_str_idx = seq[seq_idx].find(token[1],str_idx)
-            while new_str_idx == -1:
-                seq_idx+=1
-                #print("error: ", seq[seq_idx])
-                while not isinstance(seq[seq_idx],str):
-                    if tidx+1 == len(tokens):
-                        parent.append(seq[seq_idx])
-                    else:
-                        s.append(seq[seq_idx])
+            print("token[1] ",token[1])
+            print("str_idx ",str_idx)
+            print("seq[seq_idx] ",seq[seq_idx])
+            print("new_str_idx ",new_str_idx)
+            print("len(token[1]) ", len(token[1]))
+            
+            if len(token_12) != 2:
+                while new_str_idx == -1:
                     seq_idx+=1
-                    if seq_idx==len(seq):
-                        return
-                str_idx=0
-                new_str_idx = seq[seq_idx].find(token[1],str_idx)
-            str_idx = new_str_idx+len(token[1])
-            ner = token[10] 
-            if ner.startswith('B-'): #if it's the beginning of an entity
+                    #print("error: ", seq[seq_idx])
+                    while not isinstance(seq[seq_idx],str):
+                        if tidx+1 == len(tokens):
+                            parent.append(seq[seq_idx])
+                        else:
+                            s.append(seq[seq_idx])
+                        seq_idx+=1
+                        if seq_idx==len(seq):
+                            return
+                    str_idx=0
+                    new_str_idx = seq[seq_idx].find(token[1],str_idx)
+            
+            if len(token_12) != 2:
+                str_idx = new_str_idx+len(token[1])
+            print("1 str_idx", str_idx)
+            
+            if len(token_12) == 1:
+                continue
+                
+            ner = token[10]
+            flag_ner = 0
+            #print(token)
+            if ner.startswith('B-'):
                 if ne_cat!='O': 
                     s = parent_s
-                name = ET.Element('name') #we create an XML element
-                name.attrib['type'] = {'B-PER':'PER','B-LOC':'LOC','B-ORG':'ORG','B-MISC':'MISC'}[ner] #which attribute will be the value according to the dictionary specified for the ner value  
-                s.append(name) #we append the new element to the sentence
+                name = ET.Element('name')
+                name.attrib['type'] = {'B-PER':'PER','B-LOC':'LOC','B-ORG':'ORG','B-MISC':'MISC'}[ner]
+                s.append(name)
                 s = name
-                ne_cat = ner #variable del anterior para el proximo token
-            elif ner=='O' and ne_cat!='O': #ne_cat.startswith('B-')
+                ne_cat = ner
+                flag_ner = 1
+            elif ner=='O' and ne_cat!='O':
                 s = parent_s
                 ne_cat = ner
-            #nuevo
-            cont = token[11] 
-            if cont.startswith('B-'): #if it's the beginning of an entity
-                if cont_cat!='_': 
-                    s = parent_s
-                name2 = ET.Element('w') #we create an XML element
-                name2.attrib['xml:id'] = t_id + '-' + str(tidx+2) #which attribute will be the value according to the dictionary specified for the ner value  
-                s.append(name2) #we append the new element to the sentence
-                s = name2
-                cont_cat = cont
-            elif cont == '_' and cont_cat!='_':# cont_cat.startswith('B-')
+                flag_ner = 1
+            
+            
+            if len(token_12) > 0:
+                comp = ET.Element('w')
+                comp.attrib['xml:id'] = t_id
+                comp.text=token[1]
+                s.append(comp)
+                s = comp
+                for ridx in token_12:
+                    for cidx,tokens_comp in enumerate(tokens):
+                        token_comp = tokens_comp.split('\t')
+                        if int(ridx) == int(token_comp[0]):
+                            w=ET.Element('w')
+                            w.attrib['pos']=token_comp[3]
+                            w.attrib['join']='right'
+                            w.attrib['lemma']=token_comp[2]
+                            w.attrib['msd']='UPosTag='+token_comp[5]
+                            w.attrib['xml:id']=s_id+'.'+token_comp[0]
+                            w.text=token_comp[1]
+                            s.append(w)
                 s = parent_s
-                cont_cat = cont
-            #fin de lo nuevo
-            if token[3]=='PUNCT':
-                pc=ET.Element('pc',attrib={'xml:id':t_id})
+            elif flag_ner == 1:
+                w=ET.Element('w')
+                w.attrib['lemma']=token[2]
+                w.attrib['msd']='UPosTag=PROPN'
+                w.attrib['xml:id']=t_id
+                w.text=token[1]
+                s.append(w)
+            
+            elif token[3]=='PUNCT' and flag_ner == 0:
+                pc=ET.Element('pc')
+                pc.attrib['pos']='PUNCT'
                 pc.attrib['msd']='UPosTag=PUNCT'
-                pc.attrib['ana']='mte:'+token[4]
-                pc.text=token[1]
-                if 'SpaceAfter=No' in token[9]:
-                    pc.attrib['join']='right'
+                pc.attrib['xml:id']=t_id
+                pc.text=str(token[1])
                 s.append(pc)
             else:
-                w=ET.Element('w',attrib={'xml:id':t_id})
-                w.attrib['msd']='UPosTag='+token[3]
+                w=ET.Element('w')
+                if str(token[1]).startswith("'") == True:
+                    w.attrib['join']="left" 
+                w.attrib['pos']=token[3]
+                w.attrib['lemma']=token[2]
+                w.attrib['msd']='UPosTag='+token[5]
+                w.attrib['xml:id']=t_id
+                
                 if token[5]!='_':
                     w.attrib['msd']+='|'+token[5]
-                w.attrib['ana']='mte:'+token[4]
-                w.attrib['lemma']=token[2]
+               
                 w.text=token[1]
-                if 'SpaceAfter=No' in token[9]:
-                    w.attrib['join']='right'
+                #if 'SpaceAfter=No' in token[9]:
+                    #w.attrib['join']='right'
                 s.append(w)
         linkGrp=ET.Element('linkGrp',attrib={'type':'UD-SYN','targFunc':'head argument'})
         parent_s.append(linkGrp)
